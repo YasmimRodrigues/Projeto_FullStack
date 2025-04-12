ce01c526a726140fc3a30cc3d8b12b4848ddd734
Yasmim Rodrigues <145919042+YasmimRodrigues@users.noreply.github.com>
2025-04-12 15:30:09 -0300

    Delete exceptions.py

--- exceptions.py
+++
@@ -1,7 +0,0 @@
-# exceptions.py
-from fastapi import HTTPException
-
-class CustomHTTPException(HTTPException):
-    def __init__(self, detail: str, status_code: int):
-        super().__init__(status_code=status_code, detail=detail)
-
